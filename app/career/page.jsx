b8b7538a1807ab7f676d9ca7b710ceafca2c160a
unknown <imtiazzahir00@gmail.com>
2023-06-03 09:55:47 +0600

    d

--- app/career/page.jsx
+++ app/career/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Career from './Career'
-import Investor from './Investor'
-import Mission from './Mission'
-import Values from './Values'
-import Team from './Team'
+import Career from '../../components/careers/Career'
+import Investor from '../../components/careers/Investor'
+import Mission from '../../components/careers/Mission'
+import Values from '../../components/careers/Values'
+import Team from '../../components/careers/Team'
 
 
 export default function page() {
